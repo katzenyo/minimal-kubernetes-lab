94069b4c361d6f956761f309ce36ba08677af433
katzenyo <113150671+katzenyo@users.noreply.github.com>
2025-08-26 22:15:25 -0400

    Fixed missing route table, mapped public IPs on launch for SSM access

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,9 +7,29 @@ resource "aws_vpc" "primary" {
 resource "aws_subnet" "private" {
   vpc_id = aws_vpc.primary.id
   cidr_block = var.private_cidr
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw-primary" {
   vpc_id = aws_vpc.primary.id
+}
+
+### Route Tables
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.primary.id
+
+  route {
+    cidr_block = "0.0.0.0/0" # full public ingress
+    gateway_id = aws_internet_gateway.igw-primary.id
+  }
+
+  tags = {
+    Name = "public-route-to-igw"
+  }
+}
+
+resource "aws_route_table_association" "public_route" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.private.id
 }
